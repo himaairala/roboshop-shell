6799d6afd9edd33bb07613fcecdd5d7c7cc44613
Hima <himabindhuairala@gmail.com>
2023-01-28 20:36:00 +0530

    user script

--- user.sh
+++ user.sh
@@ -4,9 +4,9 @@ yum install nodejs -y
 useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-rm -rf user
 cd /home/roboshop
-unzip -e /tmp/user.zip
+rm -rf user
+unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
