49c1a7534775ed4d1086da0edc3e6f385c59a8a7
Hima <himabindhuairala@gmail.com>
2023-01-28 17:05:22 +0530

    user script

--- user.sh
+++ user.sh
@@ -4,16 +4,8 @@ yum install nodejs -y
 useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-rm -rf user
 cd /home/roboshop
-unzip -o /tmp/user.zip
+unzip -e /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
-
-sed -i -e '/REDIS_ENDPOINT/redis.himaairala/' -e '/MONGO_ENDPOINT/mongodb.himaairala/' systemd.service
-
-# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-# systemctl daemon-reload
-# systemctl start user
-# systemctl enable user
