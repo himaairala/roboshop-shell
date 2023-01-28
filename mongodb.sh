80bf5e7833a68c94dd5d4389c99719e2a7e2f113
Hima <himabindhuairala@gmail.com>
2023-01-28 16:01:59 +0530

    mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -11,7 +11,7 @@ systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
-unzip -e mongodb.zip
+unzip -o mongodb.zip
 cd mongodb-main
 mongo < catalogue.js
 mongo < users.js
