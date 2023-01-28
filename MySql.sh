23e9042cd2228e3904797e316f9d534cf881eebe
Hima <himabindhuairala@gmail.com>
2023-01-28 22:44:57 +0530

    mysql script

--- MySql.sh
+++ MySql.sh
@@ -8,7 +8,7 @@ systemctl restart mysqld
 
 
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" >/tmp/root-pass-sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 #cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"7g?clXdsI9o"
