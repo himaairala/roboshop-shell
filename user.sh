72ceec0d7202cda5375702f1ff6d5964e9cd5f68
Hima <himabindhuairala@gmail.com>
2023-01-28 16:48:33 +0530

    user script

--- user.sh
+++ user.sh
@@ -10,3 +10,10 @@ unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
+
+sed -i -e '/REDIS_ENDPOINT/redis.himaairala/' -e '/MONGO_ENDPOINT/mongodb.himaairala' systemd.service
+
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
