9d6c2eef9d2e33642ccf875255d49f7561d7d578
Hima <himabindhuairala@gmail.com>
2023-01-28 16:16:43 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,10 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 
+useradd roboshop
+
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+
 cd /home/roboshop
 rm -rf catalogue
 unzip -o /tmp/catalogue.zip
