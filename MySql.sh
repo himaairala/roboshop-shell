92e3fae210a63cb437df8f0e03b7825fdfb13623
Hima <himabindhuairala@gmail.com>
2023-02-03 22:35:21 +0530

    mysql script

--- MySql.sh
+++ MySql.sh
@@ -5,7 +5,7 @@ if [ $? -eq 0 ]; then
   else
     echo FALURE
 fi
-echo Disable MySql 8 version repo
+echo -e "\e[34mDisable MySql 8 version repo\e[0m"
 dnf module disable mysql -y
 
 if [ $? -eq 0 ]; then
@@ -13,7 +13,7 @@ if [ $? -eq 0 ]; then
   else
     echo FALURE
 fi
-echo install MySql
+echo -e "\e[35minstall MySql\e[0m"
 yum install mysql-community-server -y
 
 if [ $? -eq 0 ]; then
@@ -21,14 +21,14 @@ if [ $? -eq 0 ]; then
   else
     echo FALURE
 fi
-echo Enable MySql Service
+echo -e "\e[36mEnable MySql Service\e[0m"
 systemctl enable mysqld
 if [ $? -eq 0 ]; then
   echo SUCCESS
   else
     echo FALURE
 fi
-echo start MySql Service
+echo -e "\e[32start MySql Service\e[0m"
 systemctl restart mysqld
 if [ $? -eq 0 ]; then
   echo SUCCESS
