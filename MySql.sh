1daff38d8eca3d8d534df7de09d6ee309704cc0b
Hima <himabindhuairala@gmail.com>
2023-02-03 22:19:26 +0530

    mysql script

--- MySql.sh
+++ MySql.sh
@@ -1,11 +1,41 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo FALURE
+fi
+
 dnf module disable mysql -y
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo FALURE
+fi
 
 yum install mysql-community-server -y
 
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo FALURE
+fi
+
 systemctl enable mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo FALURE
+fi
 systemctl restart mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo FALURE
+fi
+
 echo show databases | mysql -uroot -pRoboShop@1
+
 if [ $? -ne 0 ]
 then
  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
