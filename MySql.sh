0c655f6501bf9d23c0f4214a75e4ff58f2dfbc07
Hima <himabindhuairala@gmail.com>
2023-02-04 14:29:58 +0530

    functions script

--- MySql.sh
+++ MySql.sh
@@ -5,48 +5,35 @@ if [ -z "$1" ]; then
 fi
 ROBOSHOP_MYSQL_PASSWORD=$1
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+    else
+      echo FAILURE
+      exit
+      fi
+}
+
 echo -e "\e[33mDownloading MySql Repo File\e[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-  else
-    echo FAILURE
-    exit
-fi
+STAT $?
+
 echo -e "\e[34mDisable MySql 8 version repo\e[0m"
 dnf module disable mysql -y
+STAT $?
 
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-  else
-    echo FAILURE
-    exit
-fi
 echo -e "\e[35minstall MySql\e[0m"
 yum install mysql-community-server -y
+STAT $?
 
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-  else
-    echo FAILURE
-    exit
-fi
 echo -e "\e[36mEnable MySql Service\e[0m"
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-  else
-    echo FAILURE
-    exit
-fi
+STAT $?
+
 echo -e "\e[32start MySql Service\e[0m"
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-  else
-    echo FAILURE
-    exit
-fi
+STAT $?
+
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 
