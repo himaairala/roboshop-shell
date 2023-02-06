cb4624fac96922c015f90c04f2dba2b8b8294655
Hima <himabindhuairala@gmail.com>
2023-02-06 23:10:14 +0530

    cart script

--- redis.sh
+++ redis.sh
@@ -1,8 +1,26 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-yum install redis -y
+COMPONENT=redis
+source common.sh
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+PRINT "Install redis repo"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG
+STAT $?
 
-systemctl enable redis
-systemctl start redis
+PRINT "Enable redis repo 6.2 version"
+dnf module enable redis:remi-6.2 -y &>>$LOG
+STAT $?
+
+PRINT "Install redis"
+yum install redis -y &>>$LOG
+STAT $?
+
+PRINT "Update redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>$LOG
+STAT $?
+
+PRINT "Enable redis service"
+systemctl enable redis &>>$LOG
+STAT $?
+
+PRINT "Restart redis service"
+systemctl restart redis &>>$LOG
+STAT $?
