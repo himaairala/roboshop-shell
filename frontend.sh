f415888b9dca5f83955e5b8e05ec3dc62eb16dfa
Hima <himabindhuairala@gmail.com>
2023-02-06 22:54:47 +0530

    cart script

--- frontend.sh
+++ frontend.sh
@@ -11,8 +11,16 @@ STAT $?
 APP_LOC=/usr/share/nginx/html
 
 DOWNLOAD_APP_CODE
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+mv frontend-main/static/* . &>>$LOG
 
-systemctl enable nginx
-systemctl restart nginx
+PRINT "copy roboshop configuration file"
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT $?
+
+PRINT "Enable nginx service"
+systemctl enable nginx &>>$LOG
+STAT $?
+
+PRINT "Restart nginx service"
+systemctl restart nginx &>>$LOG
+STAT $?
