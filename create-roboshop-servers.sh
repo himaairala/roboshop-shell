8cda23c372f7d000f662c8efe30234cad0502768
Hima <himabindhuairala@gmail.com>
2023-02-09 23:43:34 +0530

    roboshop-servers script

---
+++ create-roboshop-servers.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#### Change these values ###
+
+ZONE_ID="Z06349093DDNOKRYZ0JZN"
+SG_NAME="allow-all"
+
+#############################################
+
+env=dev
+
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+  --image-id $(AMI_ID) \
+  --instance-type t3.micro \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=NameValue=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=NameValue${COMPONENT}}]" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehaviour=stop}" \
+  --security-group-ids ${SGID} \
+  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  exit
+
+}
+
+##  Main Program ##
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-Devops-practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+if [ -z "${AMI_ID}" ]; then
+  echo "AMI_ID not found"
+  exit 1
+fi
+
+SGID=$(aws ec2 describe-security-grous --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+if [ -z "${SGIDID}" ]; then
+  echo "Given Security Group does not exist"
+  exit 1
+fi
+
+for component in catalogue cart user shipping frontend mongodb mysql rabbitmq redis dispatch; do
+  COMPONENT="${COMPONENT}-${env}"
+  create_ec2
+done
