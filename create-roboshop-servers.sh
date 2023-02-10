f952ea9838611a79c72581f3ee6e942f3dd954e6
Hima <himabindhuairala@gmail.com>
2023-02-10 14:46:15 +0530

    roboshop-servers script

--- create-roboshop-servers.sh
+++ create-roboshop-servers.sh
@@ -18,6 +18,8 @@ create_ec2() {
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehaviour=stop}" \
   --security-group-ids ${SGID} \
   | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+  exit
+
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
