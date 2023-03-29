2d290b80ff0896de724d267f2adf2cec40779f89
ADMIN <himaairala@gmail.com>
2023-03-29 22:56:28 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -17,6 +17,10 @@ PRINT "copy roboshop configuration file"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT $?
 
+PRINT "Update roboshop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devops321.online/' -e '/user/ s/localhost/dev-user.devops321.online/' -e '/cart/ s/localhost/dev-cart.devops321.online/' -e '/shipping/ s/localhost/dev-shipping.devops321.online/' -e '/payment/ s/localhost/dev-payment.devops321.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
+
 PRINT "Enable nginx service"
 systemctl enable nginx &>>$LOG
 STAT $?
