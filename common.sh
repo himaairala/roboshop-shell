fcf75984f9dd772b8a23861d8f82cc98483347ee
ADMIN <himaairala@gmail.com>
2023-04-09 22:41:14 +0530

    common script

--- common.sh
+++ common.sh
@@ -125,7 +125,7 @@ pip3 install -r requirements.txt &>>$LOG
 STAT $?
 USER_ID=$(id -u roboshop)
 GROUP_ID=$(id -g roboshop)
-sed -i -e "/uid/ c uid = ${USER_ID}" -e "/uid/ c uid = ${GROUP_ID}" {$COMPONENT}.ini
+sed -i -e "/uid/ c uid = ${USER_ID}" -e "/uid/ c uid = ${GROUP_ID}" ${COMPONENT}.ini
 
 SYSTEMD_SETUP
 }
